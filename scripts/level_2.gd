c94994e4618399c93ffa32a4fd4b7eb0264e9bf2
tatarchukvolodumer-dotcom <tatarchuk.v.a.-io42@edu.kpi.ua>
2026-04-12 22:13:13 +0300

    meh
    -have some trouble, remake in futer

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -16,10 +16,10 @@ func _ready():
 	Fork.body_entered.connect(Callable(self, "_on_fork_entered"))
 	Knife_horizontal.body_entered.connect(Callable(self, "_on_knife_horizontal_entered"))
 	
-	Cam.limit_left = 96
-	Cam.limit_top = 175
-	Cam.limit_right = 2040
-	Cam.limit_bottom = 1005
+	Cam.limit_left = 305
+	Cam.limit_top = 405
+	Cam.limit_right = 2300
+	Cam.limit_bottom = 1250
 	
 	
 func _on_bottom_entered(body):
